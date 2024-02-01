a6c96e71982a7d374b15b645fba282907881547e
Ricky Senft <rickysenft@gmail.com>
2024-01-31 21:59:39 -0800

    2016 Day 5 stubs

--- src/aoc/2016/2016_exercises.cpp
+++ src/aoc/2016/2016_exercises.cpp
@@ -9,6 +9,7 @@ extern const s_aoc_exercise_data k_2016_day1_exercise;
 extern const s_aoc_exercise_data k_2016_day2_exercise;
 extern const s_aoc_exercise_data k_2016_day3_exercise;
 extern const s_aoc_exercise_data k_2016_day4_exercise;
+extern const s_aoc_exercise_data k_2016_day5_exercise;
 
 extern const s_aoc_exercise_data* k_2016_exercises[] =
 {
@@ -16,6 +17,7 @@ extern const s_aoc_exercise_data* k_2016_exercises[] =
 	& k_2016_day2_exercise,
 	& k_2016_day3_exercise,
 	& k_2016_day4_exercise,
+	& k_2016_day5_exercise,
 };
 
 extern const uint32 k_2016_exercise_count = ARRAY_SIZE(k_2016_exercises);
---
+++ src/aoc/2016/day5_2016_exercise.cpp
@@ -0,0 +1,38 @@
+
+//-------------- headers
+
+#include "aoc/infrastructure/exercise.h"
+
+#include "foundation/containers/static/static_string.h"
+
+#include <iostream>
+
+//-------------- constants
+
+//-------------- prototypes
+
+void execute_2016_day5_part1(FILE* file);
+void execute_2016_day5_part2(FILE* file);
+
+//-------------- globals
+
+extern const s_aoc_exercise_data k_2016_day5_exercise
+{
+	2016,
+	5,
+	"How About a Nice Game of Chess?",
+	execute_2016_day5_part1,
+	execute_2016_day5_part2
+};
+
+//-------------- definitions
+
+void execute_2016_day5_part1(FILE* file)
+{
+	std::cout << "2016 Day 5 Part 1" << std::endl;
+}
+
+void execute_2016_day5_part2(FILE* file)
+{
+	std::cout << "2016 Day 5 Part 2" << std::endl;
+}
