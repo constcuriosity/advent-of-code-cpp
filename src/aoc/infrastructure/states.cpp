3779718cb8302f3ddbae1481e308d46d219ff706
Ricky Senft <rickysenft@gmail.com>
2024-04-28 22:41:13 -0700

    Convert AoC Infrastruture to use n_log::output
    - Also includes a small bug fix for day selection, where it wasn't returning the right range after inputting an incorrect value

--- src/aoc/infrastructure/states.cpp
+++ src/aoc/infrastructure/states.cpp
@@ -4,6 +4,7 @@
 #include "aoc/infrastructure/states.h"
 
 #include "foundation/containers/static/static_string.h"
+#include "foundation/io/log.h"
 
 #include "aoc/infrastructure/application_data.h"
 #include "aoc/infrastructure/exercise.h"
@@ -15,10 +16,8 @@
 
 void c_aoc_introduction_state::enter()
 {
-	std::cout << "Advent of Code Exercises" << std::endl;
-	std::cout << "========================" << std::endl;
-
-	std::cout << std::endl;
+	n_log::output("Advent of Code Exercises\n");
+	n_log::output("========================\n\n");
 
 	e_aoc_states next_state = e_aoc_states::execute_day;
 
@@ -36,11 +35,11 @@ void c_aoc_introduction_state::enter()
 
 void c_aoc_control_flow_state::enter()
 {
-	std::cout << "Choose an option:" << std::endl;
-	std::cout << "R - Repeat the last exercise" << std::endl;
-	std::cout << "Y - decide on a new Year" << std::endl;
-	std::cout << "D - decide on a new Day" << std::endl;
-	std::cout << "E - End your session" << std::endl;
+	n_log::output("Choose an option:\n");
+	n_log::output("R - Repeat the last exercise\n");
+	n_log::output("Y - decide on a new Year\n");
+	n_log::output("D - decide on a new Day\n");
+	n_log::output("E - End your session\n");
 }
 
 void c_aoc_control_flow_state::update()
@@ -52,7 +51,7 @@ void c_aoc_control_flow_state::update()
 	std::cin.clear();
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-	std::cout << std::endl;
+	n_log::output("\n");
 
 	if (decision == 'R' || decision == 'r')
 	{
@@ -72,20 +71,20 @@ void c_aoc_control_flow_state::update()
 	}
 	else
 	{
-		std::cout << "Input not recognized. Try again: ";
+		n_log::output("Input not recognized. Try again: ");
 	}
 }
 
 void c_aoc_select_year_state::enter()
 {
-	std::cout << "Available Years:" << std::endl;
+	n_log::output("Available Years:\n");
 
 	for (int32 year_index = 0; year_index < m_data->exercises.get_size(); year_index++)
 	{
-		std::cout << m_data->exercises[year_index].year << std::endl;
+		n_log::output("{}\n", m_data->exercises[year_index].year);
 	}
 
-	std::cout << std::endl << "Enter the year that you'd like to execute: ";
+	n_log::output("\nEnter the year that you'd like to execute: ");
 }
 
 void c_aoc_select_year_state::update()
@@ -97,7 +96,7 @@ void c_aoc_select_year_state::update()
 	std::cin.clear();
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-	std::cout << std::endl;
+	n_log::output("\n");
 
 	int32 year_index = m_data->exercises.find(
 		[&](const s_aoc_year_exercises& exercise) { return exercise.year == year; });
@@ -117,7 +116,7 @@ void c_aoc_select_year_state::update()
 	}
 	else
 	{
-		std::cout << "A valid year wasn't entered. Try again: ";
+		n_log::output("A valid year wasn't entered. Try again: ");
 	}
 }
 
@@ -128,17 +127,17 @@ void c_aoc_select_day_state::enter()
 
 	if (year_index != NONE)
 	{
-		std::cout << "Year " << m_data->year << " Exercises:" << std::endl;
+		n_log::output("Year {} Exercises:\n", m_data->year);
 
 		int32 exercise_count = m_data->exercises[year_index].exercises.get_size();
 		for (int32 exercise_index = 0; exercise_index < exercise_count; exercise_index++)
 		{
 			const s_aoc_exercise_data* exercise = m_data->exercises[year_index].exercises[exercise_index];
 
-			std::cout << "Day " << exercise->day << " - " << exercise->name << std::endl;
+			n_log::output("Day {} - {}\n", exercise->day, exercise->name);
 		}
 
-		std::cout << std::endl << "Enter the day that you'd like to execute (1 - " << exercise_count << "): ";
+		n_log::output("\nEnter the day that you'd like to execute (1 - {}): ", exercise_count);
 	}
 }
 
@@ -156,7 +155,7 @@ void c_aoc_select_day_state::update()
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-		std::cout << std::endl;
+		n_log::output("\n");
 
 		if (m_data->exercises[year_index].exercises.is_valid_index(day_index - 1))
 		{
@@ -165,13 +164,13 @@ void c_aoc_select_day_state::update()
 		}
 		else
 		{
-			int32 exercise_count = m_data->exercises.get_size();
-			std::cout << "A valid day wasn't entered. Try again (1 - " << exercise_count << "): ";
+			int32 exercise_count = m_data->exercises[year_index].exercises.get_size();
+			n_log::output("A valid day wasn't entered. Try again (1 - {}): ", exercise_count);
 		}
 	}
 	else
 	{
-		std::cout << "Can't select day because we don't have have a valid year" << std::endl << std::endl;
+		n_log::output("Can't select day because we don't have have a valid year\n\n");
 		m_transistor->request_transition(e_aoc_states::select_year);
 	}
 }
@@ -196,14 +195,14 @@ void c_aoc_execute_day_state::enter()
 			exercise_data->day,
 			exercise_data->name);
 
-		std::cout << title_string.get_string() << std::endl;
+		n_log::output("{}\n", title_string.get_string());
 
 		for (int32 char_index = 0; char_index < title_string.length(); char_index++)
 		{
-			std::cout << "-";
+			n_log::output("-");
 		}
 
-		std::cout << std::endl;
+		n_log::output("\n");
 
 		c_aoc_exercise_runner exercise_runner(*exercise_data);
 		exercise_runner.initialize();
@@ -211,11 +210,11 @@ void c_aoc_execute_day_state::enter()
 		exercise_runner.execute_part_two();
 		exercise_runner.dispose();
 
-		std::cout << "Exercise Complete!" << std::endl << std::endl;
+		n_log::output("Exercise Complete!\n\n");
 	}
 	else
 	{
-		std::cout << "Couldn't find exercise data for " << m_data->year << " day " << m_data->day << std::endl;
+		n_log::output("Couldn't find exercise data for {} day {}\n", m_data->year, m_data->day);
 	}
 
 	m_transistor->request_transition(e_aoc_states::control_flow);
@@ -223,5 +222,5 @@ void c_aoc_execute_day_state::enter()
 
 void c_aoc_terminate_state::enter()
 {
-	std::cout << "Goodbye!" << std::endl;
+	n_log::output("Goodbye!\n");
 }
