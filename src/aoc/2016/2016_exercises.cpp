aed30beb406e9598effd3d557fc975c623a1bcc3
Ricky Senft <rickysenft@gmail.com>
2024-06-30 22:45:29 -0700

    Add stub for 2016 day 16

--- src/aoc/2016/2016_exercises.cpp
+++ src/aoc/2016/2016_exercises.cpp
@@ -20,6 +20,7 @@ extern const s_aoc_exercise_data k_2016_day12_exercise;
 extern const s_aoc_exercise_data k_2016_day13_exercise;
 extern const s_aoc_exercise_data k_2016_day14_exercise;
 extern const s_aoc_exercise_data k_2016_day15_exercise;
+extern const s_aoc_exercise_data k_2016_day16_exercise;
 
 extern const s_aoc_exercise_data* k_2016_exercises[] =
 {
@@ -38,6 +39,7 @@ extern const s_aoc_exercise_data* k_2016_exercises[] =
 	& k_2016_day13_exercise,
 	& k_2016_day14_exercise,
 	& k_2016_day15_exercise,
+	& k_2016_day16_exercise,
 };
 
 extern const uint32 k_2016_exercise_count = ARRAY_SIZE(k_2016_exercises);
---
+++ src/aoc/2016/day16_2016_exercise.cpp
@@ -0,0 +1,36 @@
+
+//-------------- headers
+
+#include "aoc/infrastructure/exercise.h"
+
+#include "foundation/io/log.h"
+
+//-------------- constants
+
+//-------------- prototypes
+
+void execute_2016_day16_part1(FILE* file);
+void execute_2016_day16_part2(FILE* file);
+
+//-------------- globals
+
+extern const s_aoc_exercise_data k_2016_day16_exercise
+{
+	2016,
+	16,
+	"Dragon Checksum",
+	execute_2016_day16_part1,
+	execute_2016_day16_part2
+};
+
+//-------------- definitions
+
+void execute_2016_day16_part1(FILE* file)
+{
+	n_log::output("Day 16 Part 1\n");
+}
+
+void execute_2016_day16_part2(FILE* file)
+{
+	n_log::output("Day 16 Part 2\n");
+}
